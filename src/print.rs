f97cab8ad650a4235b30d8c47892b5d4c306998f
KausaChangs <changayakausa@gmail.com>
2023-06-08 11:16:47 +0200

    learned about positional arguments

--- src/print.rs
+++ src/print.rs
@@ -2,5 +2,10 @@ pub fn run(){
     //print to console
     println!("Hello from the print.rs file");
 
-    println!("{} is from {}", "Brad", "Skateboarding")
+
+   //basic formatting
+    println!("{} is from {}", "Brad", "Skateboarding");
+
+   //Positional Arguments
+   println!("{0} is form {1} and {0} likes to {2}","Brad","Skateboarding", "Code");
 }
