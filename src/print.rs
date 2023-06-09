7890f044bb7f9a23cd2c02312f39e250b2a9cd99
KausaChangs <changayakausa@gmail.com>
2023-06-09 09:14:55 +0200

    used named arguments/parameters

--- src/print.rs
+++ src/print.rs
@@ -8,4 +8,6 @@ pub fn run(){
 
    //Positional Arguments
    println!("{0} is form {1} and {0} likes to {2}","Brad","Skateboarding", "Code");
+   //Named Arguments
+   println!("{name} likes to play {activity}", name="Kausa", activity="video games");
 }
