a9bb26476cde70e5e4a4ab618ee5bf80936a4b47
KausaChangs <changayakausa@gmail.com>
2023-06-10 19:43:08 +0200

    tried out placeholder traits

--- src/print.rs
+++ src/print.rs
@@ -1,13 +1,12 @@
 pub fn run(){
     //print to console
     println!("Hello from the print.rs file");
-
-
    //basic formatting
     println!("{} is from {}", "Brad", "Skateboarding");
-
    //Positional Arguments
    println!("{0} is form {1} and {0} likes to {2}","Brad","Skateboarding", "Code");
    //Named Arguments
    println!("{name} likes to play {activity}", name="Kausa", activity="video games");
+   //PlaceHolder traits 
+   println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
 }
