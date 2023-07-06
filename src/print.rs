1eba5030abc04056abff0358a9662ef4f6c03fd3
KausaChangs <changayakausa@gmail.com>
2023-07-06 10:18:05 +0200

    used placeholder traits

--- src/print.rs
+++ src/print.rs
@@ -4,9 +4,13 @@ pub fn run(){
    //basic formatting
     println!("{} is from {}", "Brad", "Skateboarding");
    //Positional Arguments
-   println!("{0} is form {1} and {0} likes to {2}","Brad","Skateboarding", "Code");
+   println!("{0} is from {1} and {0} likes to {2}","Brad","Skateboarding", "Code");
    //Named Arguments
    println!("{name} likes to play {activity}", name="Kausa", activity="video games");
    //PlaceHolder traits 
    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+
+   //Placeholder for debug trait
+   println!("{:?}", (12, true, "Hello"))
 }
