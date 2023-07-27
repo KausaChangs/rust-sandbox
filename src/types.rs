b5536e44852b2574d1f86f7fdc6b94133eab22f6
KausaChangs <changayakausa@gmail.com>
2023-07-27 16:35:40 +0200

    learned how to add explicit types

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,13 @@
-pub fun run(){
-    
+pub fn run(){
+
+    //default is "i32"
+    let x  = 1;
+
+    //default is "i64"
+
+    let y = 2.5;
+
+    //add explicit type
+    let y: i64 = 454545454545;
+
 }
